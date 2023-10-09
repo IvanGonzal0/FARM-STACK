ad01beb6ca6064e3d8382ee40c992386f0e21a37
Ivan Gonzalo Tapia <108291957+IvanGonzal0@users.noreply.github.com>
2023-10-09 12:45:49 -0300

    DB

---
+++ BACKEND/database.py
@@ -0,0 +1,25 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+from models import Task
+
+
+client = AsyncIOMotorClient('mongodb://localhost')
+database = client.taskdatabase
+collection = database.tasks
+
+async def get_all_task():
+    tasks = []
+    cursor = await collection.find({})
+
+    async for document in cursor:
+        tasks.append(**document)
+    return tasks
+
+async def get_one_task_id(id):
+    task = await collection.find_one({'_id': id})
+    return task
+
+
+async def create_task(id):
+    new_task = await collection.insert_one({'_id': id})
+    created_task = await collection.find_one({'_id': new_task.inserted_id})
+    return created_task
