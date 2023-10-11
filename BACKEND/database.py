f2ce129a3376ad9ea8dc666be9f0f872a56f49c3
Ivan Gonzalo Tapia <108291957+IvanGonzal0@users.noreply.github.com>
2023-10-11 16:09:02 -0300

    Backend
    Update functions

--- BACKEND/database.py
+++ BACKEND/database.py
@@ -6,6 +6,8 @@ client = AsyncIOMotorClient('mongodb://localhost')
 database = client.taskdatabase
 collection = database.tasks
 
+#CRUD
+#Get all tasks
 async def get_all_task():
     tasks = []
     cursor = await collection.find({})
@@ -13,13 +15,21 @@ async def get_all_task():
     async for document in cursor:
         tasks.append(**document)
     return tasks
-
+#Get one task
 async def get_one_task_id(id):
     task = await collection.find_one({'_id': id})
     return task
-
-
+#Create task
 async def create_task(id):
     new_task = await collection.insert_one({'_id': id})
     created_task = await collection.find_one({'_id': new_task.inserted_id})
     return created_task
+#Update task
+async def update_task(id: str,  task):
+    await collection.update_one({'_id': id}, {'$set': task})
+    document = await collection.find_one({'_id': id})
+    return document
+#Delete task
+async def delete_task(id: str):
+    await collection.delete_one({'_id': id})
+    return True
